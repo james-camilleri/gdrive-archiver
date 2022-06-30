96a0bf3ab3cf583f0efb43c377b428aa9bb65e62
James Camilleri <james@james.mt>
2022-06-30 19:19:27 +0100

    Add local context menu batch script

---
+++ context-menu/archive.cmd
@@ -0,0 +1,18 @@
+@echo off
+setlocal enabledelayedexpansion
+
+7z a %1.__archive.7z %1\* -r -mx=9
+7z t %1.__archive.7z
+
+if %errorlevel% == 0 (
+  for /f "skip=1 tokens=* delims=" %%# in ('certutil -hashfile %1.__archive.7z MD5') do (
+    if not defined md5 (
+      set md5=%%#
+    )
+  )
+
+  move %1.__archive.7z %1.!md5!.archive.7z
+  rmdir /s /q %1
+)
+
+endlocal
